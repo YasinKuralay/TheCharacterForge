74a354719dbf90d594cf49f26cd56f463ae97648
Yasin Kuralay <yasinkuralay@gmail.com>
2020-09-18 17:11:30 +0200

    BUG FIXES: Fixed a bug where using BrowserRouter caused unexpected results.

--- src/entrypage.js
+++ src/entrypage.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { Router, Link } from "react-router-dom";
 
 export default function Entrypage() {
   return (
     <div>
-      <BrowserRouter>
-        <Link to="/browseCharacters">
-          <div id="browseCharacters">
-            <h2>Browse Your Characters</h2>
-            <i className="fas fa-search fa-3x"></i>
-          </div>
-        </Link>
+      <Link to="/browseCharacters">
+        <div id="browseCharacters">
+          <h2>Browse Your Characters</h2>
+          <i className="fas fa-search fa-3x"></i>
+        </div>
+      </Link>
 
-        <Link to="/createcharacter">
-          <div id="createCharacters">
-            <h2>Create New Character</h2>
-            <i className="fas fa-hat-wizard fa-3x"></i>
-          </div>
-        </Link>
-      </BrowserRouter>
+      <Link to="/createcharacter">
+        <div id="createCharacters">
+          <h2>Create New Character</h2>
+          <i className="fas fa-hat-wizard fa-3x"></i>
+        </div>
+      </Link>
     </div>
   );
 }
