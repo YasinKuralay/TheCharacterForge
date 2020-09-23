ad09abb70c17b05da5ab34296117be4db0565055
Yasin Kuralay <yasinkuralay@gmail.com>
2020-09-23 19:43:12 +0200

    TESTING: Added the first snapshot, works fine. Going to try to eliminate the "dont use <Link> outside a <Router>" error now.

--- src/browsecharacters.test.js
+++ src/browsecharacters.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import BrowseCharacters, { randomAvatar } from "./browsecharacters.js";
 
 it("renders the BrowseCharacters component without crashing", () => {
@@ -16,4 +17,11 @@ it("returns src in the correct format /avatar{number}.png", () => {
     expect(randomAvatar()).not.toMatch(/^\/avatar\s*\.png$/);
     expect(randomAvatar()).not.toMatch(/^\/avatar\W*\.png$/);
     expect(randomAvatar()).not.toMatch(/^\/avatar\d+\.$/);
+});
+
+it("matches the previous snapshot", () => {
+    const BrowseCharactersSnapshot = renderer
+        .create(<BrowseCharacters />)
+        .toJSON();
+    expect(BrowseCharactersSnapshot).toMatchSnapshot();
 });
