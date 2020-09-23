a71b4d0a05a233bd83dedefa7126604b74cd6ffe
Yasin Kuralay <yasinkuralay@gmail.com>
2020-09-23 14:45:52 +0200

    TESTS: Adding additional expectations to tests.

--- src/browsecharacters.test.js
+++ src/browsecharacters.test.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import BrowseCharacters, { randomAvatar } from "./browsecharacters.js";
 
 it("renders the BrowseCharacters component without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<BrowseCharacters />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement("div");
+    ReactDOM.render(<BrowseCharacters />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
 
 it("returns src in the correct format /avatar{number}.png", () => {
-  expect(randomAvatar()).toMatch(/\/avatar\d+\.png/);
+    expect(randomAvatar()).toMatch(/^\/avatar\d+\.png$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\d+\.pngg$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\d+png$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\.png$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\s*\.png$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\W*\.png$/);
+    expect(randomAvatar()).not.toMatch(/^\/avatar\d+\.$/);
 });
